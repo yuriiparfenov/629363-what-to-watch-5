2f6d5110e8d183c68af074670eb65fb616218d58
parfenovyura <parfenovyura@inbox.ru>
2020-10-10 22:41:18 +0500

    Роутинг

---
+++ src/components/movie-page-review/movie-page-review.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+
+const MoviePageReview = () => {
+  return (
+    <section className="movie-card movie-card--full">
+      <div className="movie-card__hero">
+        <div className="movie-card__bg">
+          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header movie-card__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </div>
+          </div>
+        </header>
+
+        <div className="movie-card__wrap">
+          <div className="movie-card__desc">
+            <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">Drama</span>
+              <span className="movie-card__year">2014</span>
+            </p>
+
+            <div className="movie-card__buttons">
+              <button className="btn btn--play movie-card__button" type="button">
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list movie-card__button" type="button">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
+              <a href="add-review.html" className="btn movie-card__button">Add review</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="movie-card__wrap movie-card__translate-top">
+        <div className="movie-card__info">
+          <div className="movie-card__poster movie-card__poster--big">
+            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+          </div>
+
+          <div className="movie-card__desc">
+            <nav className="movie-nav movie-card__nav">
+              <ul className="movie-nav__list">
+                <li className="movie-nav__item">
+                  <a href="#" className="movie-nav__link">Overview</a>
+                </li>
+                <li className="movie-nav__item">
+                  <a href="#" className="movie-nav__link">Details</a>
+                </li>
+                <li className="movie-nav__item movie-nav__item--active">
+                  <a href="#" className="movie-nav__link">Reviews</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="movie-card__reviews movie-card__row">
+              <div className="movie-card__reviews-col">
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Kate Muir</cite>
+                      <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">8,9</div>
+                </div>
+
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Bill Goodykoontz</cite>
+                      <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">8,0</div>
+                </div>
+
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Amanda Greever</cite>
+                      <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">8,0</div>
+                </div>
+              </div>
+              <div className="movie-card__reviews-col">
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Matthew Lickona</cite>
+                      <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">7,2</div>
+                </div>
+
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Paula Fleri-Soler</cite>
+                      <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">7,6</div>
+                </div>
+
+                <div className="review">
+                  <blockquote className="review__quote">
+                    <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
+
+                    <footer className="review__details">
+                      <cite className="review__author">Paula Fleri-Soler</cite>
+                      <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                    </footer>
+                  </blockquote>
+
+                  <div className="review__rating">7,0</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MoviePageReview;
